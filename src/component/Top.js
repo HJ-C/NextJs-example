bfeff49aef87e6cedaaf32dee9580c7927875e2f
HJ-C <swch56@naver.com>
2022-04-24 23:56:14 +0900

    1

---
+++ src/component/Top.js
@@ -0,0 +1,8 @@
+import { Header } from "semantic-ui-react";
+
+export default function Top(){
+    return <div>
+        <img src="/imgages/1.png" alt="logo"></img>
+        <Header as="h1">코딩</Header>
+    </div>
+}
